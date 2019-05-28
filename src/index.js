d680b960211c11f968f6d4d1743818c161a1d442
Rattleball <13022020108@umt.edu.pk>
2019-05-28 06:50:05 +0500

    qw

--- src/index.js
+++ src/index.js
@@ -6,16 +6,11 @@ import { Router, Route, Switch } from "react-router-dom";
 import "assets/scss/material-kit-react.scss?v=1.4.0";
 
 // pages for this product
-import Components from "views/Components/Components.jsx";
-import LandingPage from "views/LandingPage/LandingPage.jsx";
-import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
 import LoginMainPage from "views/MainPage/LoginMainPage.jsx";
-import LoginPage from "views/LoginPage/LoginPage.jsx";
 import landingStudent from "views/Student/landingStudent.jsx";
 import landingTeacher from "views/Teacher/landingTeacher.jsx";
 import AdminDashboard from "views/Admin/AdminDashboard.jsx";
 import SubjectHome from "views/Subject/subjectHome.jsx";
-import Uploading from "views/uploading.jsx";
 import { SnackbarProvider } from 'notistack';
 
 
@@ -27,12 +22,8 @@ ReactDOM.render(
     }}>
   <Router history={history}>
     <Switch>
-      <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} />
       <Route path="/home-student" component={landingStudent} />
       <Route path="/home-teacher" component={landingTeacher} />
-      <Route path="/login-page" component={LoginPage} />
-      <Route path="/components" component={Components} />
       <Route path="/subject-home" component={SubjectHome} />
       <Route path="/home-admin" component={AdminDashboard} />
       <Route path="/" component={LoginMainPage} />
